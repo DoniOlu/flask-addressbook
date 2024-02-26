9748db619ea0e281c1dfc17049b767f8ee661527
doniiiid <donipreston23@gmail.com>
2024-02-25 22:45:08 -0600

    KAN-3: BE: GET API for fetching all records
    added route to fetch all contacts, and started ground work for the remaining routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
-from flask import Flask, render_template, jsonify
+import psycopg2, psycopg2.extras
+from flask import Flask, render_template, jsonify, make_response
 
 app = Flask(__name__, static_url_path='', static_folder='../app/build', template_folder='../app/build'
             )
@@ -26,3 +26,34 @@ def get_db_connection():
 @app.route('/')
 def hello():
     return app.send_static_file("index.html")
+
+@app.route("/contact/users", methods=["GET"])
+def get_all_contacts():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute('SELECT * FROM ADDRESS_BOOK;')
+        contacts = cur.fetchall()
+        cur.close()
+        conn.close()
+        return {'status': 200, 'data': contacts}
+    except:
+        return {'status': 500}
+
+# @app.route("/contact/<user_id>", methods=["GET"])
+# def get_contact(user_id):
+#     return {'status': 200}
+
+# @app.route("/contact/add", methods=["PUT"])
+# def add_contact(user):
+#     return {'status': 200}
+
+# @app.route("/contact/edit", methods=["POST"])
+# def edit_contact(user):
+#     return {'status': 200}
+
+# @app.route("/contact/delete/<user_id>", methods=["DELETE"])
+# def delete_contact(user):
+#     return {'status': 200}
+    
+
